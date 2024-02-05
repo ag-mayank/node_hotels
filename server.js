718c652c4279dc56e42f8130f9de48af562cd592
may-ag <mayag@gmail.com>
2024-02-05 19:51:40 +0530

    shift mongodb to online server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express=require("express");
 const app=express();
 const db=require("./db");
+require('dotenv').config();
 
 const bodyParser = require("body-parser")
 app.use(bodyParser.json())
@@ -22,7 +23,7 @@ const menuRoutes=require('./routes/menuRoutes')
 app.use('/menu',menuRoutes)
 
 
-
+const PORT=process.env.PORT || 3000;
 
 app.get('/',function(req,res){
     res.send("Hii!!! We welcome You")
@@ -30,6 +31,6 @@ app.get('/',function(req,res){
 
 
 
-app.listen(3000,()=>{
+app.listen(PORT,()=>{
     console.log("Server Is Active")
 }) 
